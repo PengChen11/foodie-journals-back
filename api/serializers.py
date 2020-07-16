e5d80ef33d8d44c248fb5f6934f82049b69ef8b8
Peng Chen <pchen0816@outlook.com>
2020-07-15 17:19:40 -0700

    bug fix

--- api/serializers.py
+++ api/serializers.py
@@ -7,4 +7,4 @@ from .models import Receipe
 class ReceipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receipe
-        fields = ('id', 'title', 'img_src_1', 'img_src_2', 'img_src_3', 'steps', 'ingredients', 'meal_type')
+        fields = ('id', 'title', 'img_src_1', 'img_src_2', 'img_src_3', 'steps', 'ingredients', 'meal_type','description','difficulty','created_at', 'updated_at', 'author')
